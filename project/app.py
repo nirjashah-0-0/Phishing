9f12520f2ff6f66c8e625ead0afe983221bf52ca
Nirja Shah <71543437+nirjashah-0-0@users.noreply.github.com>
2025-04-27 19:13:46 -0400

    Update app.py

--- project/app.py
+++ project/app.py
@@ -25,6 +25,9 @@ X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, 
 rf = RandomForestClassifier(n_estimators=100, random_state=42)
 rf.fit(X_train, y_train)
 
+
+features = {col: df[col].mean() for col in expected_cols}
+
 # Feature Extraction Function
 def extract_features(url):
     parsed = urlparse(url)
